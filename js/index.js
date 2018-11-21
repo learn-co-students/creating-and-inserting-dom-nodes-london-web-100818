f0118242cb30b178a30f395e60b15a66e92d9171
lola <slonanuga@gmail.com>
2018-11-21 21:21:17 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,46 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+
+element.innerHTML = 'Hello, DOM!';
+element.style.backgroundColor = '#f9f9f9';
+"#f9f9f9"
+document.body.appendChild(element);
+
+element.style.textAlign = 'center';
+
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+ 
+
+var ul = document.createElement('ul');
+ 
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+ 
+
+
+var text = document.createElement('p')
+
+
+element.appendChild(text)
+
+text.innerHTML = "Hey Girl lovely to meet ya"
+
+text.style.color = '#00000d'
+
+img = document.createElement('img')
+<img>​
+img.src = 'https://res.cloudinary.com/feelunique-com/image/upload/f_auto,t_product_large/v1/live/product/main-image/76844/mxbsc7raaadbzyi6u1nr.jpg'
+"https://res.cloudinary.com/feelunique-com/image/upload/f_auto,t_product_large/v1/live/product/main-image/76844/mxbsc7raaadbzyi6u1nr.jpg"
+element.appendChild(img)
+random = document.createElement('p')
+
+random.innterHTML = 'jajajajaja'
+
+)
