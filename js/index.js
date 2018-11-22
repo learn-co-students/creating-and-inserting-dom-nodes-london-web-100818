5551b9c7e2db74391bff7c01a209f811db121fea
github username <github email address>
2018-11-22 15:43:18 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
-//this file is optional
-//you can use this to paste in js code and have it run when index.html is loaded
+const div=document.createElement('div')
+div.innerHTML="Hello!"
+document.body.appendChild(div)
+div.style.textAlign='center'
+let ul=document.createElement('ul')
+for(i=0;i<3;i++){
+    let li=document.createElement('li');
+    li.innerText="wassssup!"
+    ul.appendChild(li)
+}
+div.appendChild(ul)
